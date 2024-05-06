59a482457c8fb63197e7dbf0aeefe19a0cb1108f
unknown <gh.lee@visitandregular.com>
2024-05-06 20:07:45 +0900

    study

---
+++ study3/lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
+        //도화지 위젯
+        appBar: AppBar(
+          title: Text('Study to Container!'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Body(),
+      ),
+    ),
+  ));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(index: 5)
+      ]
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final index;
+
+  const ExampleStateful({required this.index, super.key});
+  
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    textEditingController = TextEditingController();
+  }
+
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if(_index == 5) {
+              _index = 0;
+              return;
+            }
+            _index++;
+          });
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(_index/5),
+          child: Center(
+            child: Text('$_index')
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
